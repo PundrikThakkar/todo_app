9827ff8f39f48ef5c9b2c59ace81055f748a8b58
PundrikThakkar <pundrikpundrik3@gmail.com>
2024-02-06 22:03:57 +0530

    adding backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CreateTodo from "./components/CreateTodo";
+import Todos from "./components/Todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  fetch("http://localhost:3000/todos").then(async (res) => {
+    const json = await res.json();
+    setTodos(json.todos);
+  });
   return (
     <div>
       <CreateTodo />
+      <Todos todos={todos} />
     </div>
   );
 }
