bf04c64b9e91cee0ba4f8ac6941b4d655d92ee7f
PundrikThakkar <pundrikpundrik3@gmail.com>
2024-02-06 22:08:30 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
+const cors = require("cors");
 const port = process.env.port || 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.post("/todo", async (req, res) => {
   const createPayload = req.body;
